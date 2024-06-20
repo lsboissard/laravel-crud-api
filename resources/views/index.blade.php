0bf296f99b6af24f180ced216e319b43e5695bfc
Leo Boissard <lsboissard@gmail.com>
2024-06-20 18:47:42 -0300

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>...</title>
+  </head>
+
+  <body>
+    <h1>...</h1>
+  </body>
+
+</html>
